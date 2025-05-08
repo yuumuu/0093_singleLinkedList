6815e4fdc62f6b005eec9a80f17632a609c821f3
yuumuu <haidar.alhazen93@gmail.com>
2025-05-08 19:31:21 +0700

    Adding Case 2 on SwitchCase main Function - singly_linkedlist.cpp

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -116,6 +116,22 @@ int main() {
                 mhs.addNode();
                 break;
             }
+            case '2':
+            {
+                if (mhs.listEmpty()) {
+                    cout << endl << "List Kosong" << endl;
+                    break;
+                }
+                cout << endl << "\nMasukkan no mahasiswa yang akan dihapus: ";
+                cin >> nim;
+                
+                if (mhs.delNode(nim) == false) {
+                    cout << endl << "Data tidak ditemukan" << endl;
+                } else {
+                    cout << endl << "Data dengan nomor mahasiswa " << nim << " berhasil dihapus" << endl;
+                }
+            }
+            break;
             default:
             {
                 cout << "\nPilihan salah !\n";
