1fe921bbb7d615cfb8474c62ada88ea03c3f750c
yuumuu <haidar.alhazen93@gmail.com>
2025-05-08 19:00:39 +0700

    Adding Another Half Logic on addNode Procedure - singly_linkedlist.cpp

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -36,6 +36,18 @@ class LinkedList {
 
             Node *previous = START;
             Node *current = START;
+
+            while (current != NULL && nim >= current->noMhs) {
+                if (nim == current->noMhs) {
+                    cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                    return;
+                }
+                previous = current;
+                current = current->next;
+            }
+
+            nodeBaru->next = current;
+            previous->next = nodeBaru;
         }
 };
 
