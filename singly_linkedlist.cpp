8155a34959841ba1df5e904a050eeea1a1bb3e0f
yuumuu <haidar.alhazen93@gmail.com>
2025-05-08 19:01:39 +0700

    Adding new search Function - singly_linkedlist.cpp

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -53,6 +53,18 @@ class LinkedList {
         bool listEmpty() {
             return (START == NULL);
         }
+
+        bool search(int nim, Node **previous, Node **current) {
+            *previous = START;
+            *current = START;
+
+            while (*current != NULL && nim != (*current)->noMhs) {
+                *previous = *current;
+                *current = (*current)->next;
+            }
+
+            return (*current != NULL);
+        }
 };
 
 int main() {
