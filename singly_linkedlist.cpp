32be62bcba2730d7558ef714036b06784f9d5d6a
yuumuu <haidar.alhazen93@gmail.com>
2025-05-08 18:57:35 +0700

    Adding Half Logic on addNode Procedure - singly_linkedlist.cpp

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -15,6 +15,28 @@ class LinkedList {
         LinkedList() {
             START = NULL;
         }
+
+        void addNode() {
+            int nim;
+            cout << "\nMasukkan Nomor Mahasiswa: ";
+            cin >> nim;
+
+            Node *nodeBaru = new Node();
+            nodeBaru->noMhs = nim;
+
+            if (START == NULL || nim <= START->noMhs) {
+                if (START != NULL && nim == START->noMhs) {
+                    cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                    return;
+                }
+                nodeBaru->next = START;
+                START = nodeBaru;
+                return;
+            }
+
+            Node *previous = START;
+            Node *current = START;
+        }
 };
 
 int main() {
