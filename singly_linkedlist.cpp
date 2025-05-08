6b2e9422bc1ab22c7b0cbf1a0ee49a954ae038a7
yuumuu <haidar.alhazen93@gmail.com>
2025-05-08 19:02:34 +0700

    Adding new delNode Function - singly_linkedlist.cpp

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -65,6 +65,20 @@ class LinkedList {
 
             return (*current != NULL);
         }
+
+        bool delNode(int nim) {
+            Node *current, *previous;
+            if (!search(nim, &previous, &current))
+                return false;
+
+            if (current == START)
+                START = START->next;
+            else
+                previous->next = current->next;
+
+            delete current;
+            return true;
+        }
 };
 
 int main() {
